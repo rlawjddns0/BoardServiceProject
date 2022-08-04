e0d9ec667a15c91fb5f0bfaf2487456a06dd5aca
KJW <rlawjddns0@naver.com>
2022-08-05 00:32:21 +0900

    게시판 서비스 개발 시작

---
+++ src/main/java/com/example/boardService/BoardServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.boardService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardServiceApplication.class, args);
+
+
+	}
+
+}
---
+++ src/main/java/com/example/boardService/controller/ApiController.java
@@ -0,0 +1,10 @@
+package com.example.boardService.controller;
+
+public class ApiController {
+
+
+
+
+
+
+}
