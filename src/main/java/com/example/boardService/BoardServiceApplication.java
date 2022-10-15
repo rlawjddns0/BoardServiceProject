4100c90fc724e7f113c387eb97d1c8ebe350c7e6
KJW <rlawjddns0@naver.com>
2022-10-15 14:44:10 +0900

    #14 h2-console 설정 삭제, 시큐리티 제외
    사용안하니까 삭제합니다, 시큐리티 귀찮아서 삭제

--- src/main/java/com/example/boardService/BoardServiceApplication.java
+++ src/main/java/com/example/boardService/BoardServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.boardService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BoardServiceApplication {
 
 	public static void main(String[] args) {
